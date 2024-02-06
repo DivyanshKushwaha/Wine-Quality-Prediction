3674042ea346ef27119d322102b260c8d97b898d
Divyansh Kushwaha <jatinkushwaha309@gmail.com>
2024-02-06 20:25:42 +0530

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from projectml import logger
-logger.info("Welcome to our custom logging")
+from projectml.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
